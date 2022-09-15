b04f8bd76ae85a06566e254ef4fc0c59b6634c5e
ngseke <ngseke@gmail.com>
2022-09-15 22:09:27 +0800

    feat: create Cover

---
+++ src/components/Cover.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const style = {
+  backgroundImage: `url(${'https://images.unsplash.com/photo-1580397581145-cdb6a35b7d3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1930&q=80'})`,
+}
+</script>
+
+<template>
+  <header
+    class="h-screen bg-cover bg-center"
+    :style="style"
+  >
+    <slot />
+  </header>
+</template>
