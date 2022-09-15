dc0662dc7fb8d37b0f0b68dd1485ee89f4f0ded5
ngseke <ngseke@gmail.com>
2022-09-15 21:54:23 +0800

    chore: setup vite TypeScript checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     eslint(),
+    checker({ typescript: true, vueTsc: true }),
   ],
 })
