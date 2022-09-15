09b7fe56e146b392392e8c05e37646b455f8fb58
ngseke <ngseke@gmail.com>
2022-09-15 21:51:55 +0800

    chore: setup TailwindCSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
