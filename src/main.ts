449ad97f8a529f7718cbe2003f24933af58d4abe
ngseke <ngseke@gmail.com>
2022-09-18 14:01:37 +0800

    chore: setup FontAwesome

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,12 @@ import './index.sass'
 
 import App from './App.vue'
 import routes from './routes'
+import { FontAwesomeIcon } from './font-awesome'
 
-export const createApp = ViteSSG(App, { routes })
+export const createApp = ViteSSG(
+  App,
+  { routes },
+  ({ app }) => {
+    app.component('Fa', FontAwesomeIcon)
+  }
+)
