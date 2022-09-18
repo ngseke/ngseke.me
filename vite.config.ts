36ad86f8aca3114f1cc2dbe47e2529df5955355c
ngseke <ngseke@gmail.com>
2022-09-19 00:44:28 +0800

    chore: setup vite-plugin-md

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,21 @@ import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import checker from 'vite-plugin-checker'
 import Pages from 'vite-plugin-pages'
+import Markdown from 'vite-plugin-md'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [
-    vue(),
+    vue({
+      include: [/\.vue$/, /\.md$/],
+      reactivityTransform: true,
+    }),
     eslint(),
     checker({ typescript: true, vueTsc: true }),
-    Pages(),
+    Pages({
+      extensions: ['vue', 'md'],
+    }),
+    Markdown(),
   ],
 })
