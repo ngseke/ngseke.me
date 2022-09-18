258473db62a1ea45a987a91073ea100a110beb8b
ngseke <ngseke@gmail.com>
2022-09-19 00:52:18 +0800

    feat: create project route

---
+++ src/pages/project.vue
@@ -0,0 +1,14 @@
+<route lang="yaml">
+name: "project"
+</route>
+
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container px-4 pt-16">
+    <div class="mx-auto max-w-5xl py-16">
+      <RouterView />
+    </div>
+  </div>
+</template>
--- src/pages/project/index.vue
+++ src/pages/project/index.vue
@@ -1,44 +1,4 @@
 <route lang="yaml">
-name: "projects"
+redirect:
+  name: 'projects'
 </route>
-
-<script setup lang="ts">
-import { useHead } from '@vueuse/head'
-import TitleCategory from '../../components/TitleCategory.vue'
-import CardProject from '../../components/CardProject.vue'
-import projects from '../../modules/projects'
-
-useHead({
-  title: `Projects | ${import.meta.env.VITE_SITE_NAME}`,
-})
-</script>
-
-<template>
-  <div class="container px-4 pt-16">
-    <div class="mx-auto max-w-5xl py-16">
-      <section
-        v-for="({ title, list, cover }, index) in projects"
-        :key="index"
-        class="mb-16 space-y-8"
-      >
-        <TitleCategory>{{ title }}</TitleCategory>
-        <ul class="-mx-4 flex flex-wrap">
-          <li
-            v-for="({ name, title, description, github, link }, index) in list"
-            :key="index"
-            class="mb-8 w-full px-4 md:w-1/2"
-          >
-            <CardProject
-              :title="title"
-              :name="name"
-              :description="description"
-              :github="github"
-              :link="link"
-              :img="cover"
-            />
-          </li>
-        </ul>
-      </section>
-    </div>
-  </div>
-</template>
