ccf2588250e57bd8ff6bed3f98b81a7bdb952551
ngseke <ngseke@gmail.com>
2022-09-25 00:28:55 +0800

    refactor: extract component Prose

---
+++ src/components/Prose.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <article class="prose">
+      <slot />
+    </article>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+=media
+  max-width: min(30rem, 100%)
+
+:deep(.prose)
+  @apply mx-auto
+  h2
+    @apply mb-4 text-2xl font-medium md:text-3xl
+    &:not(:first-child)
+      @apply mt-12
+  h3
+    @apply mb-4 text-xl font-medium
+
+  p
+    @apply leading-7 mb-6
+
+  ul, ol
+    @apply list-outside pl-5 mb-6 space-y-1
+    li
+      @apply pl-2
+  ul
+    @apply list-disc
+  ol
+    @apply list-decimal
+
+  p > img
+    @apply mx-auto w-auto mb-4
+    +media
+  a
+    @apply underline
+  hr
+    @apply my-6 border-current opacity-30
+  .embed-responsive
+    @apply mb-12 mx-auto w-full relative overflow-hidden after:pt-[56.25%] after:block
+    +media
+    .embed-responsive-item
+      @apply absolute inset-0 h-full w-full
+  </style>
--- src/pages/project.vue
+++ src/pages/project.vue
@@ -11,6 +11,7 @@ import { VITE_SITE_NAME, VITE_SITE_ORIGIN } from '../modules/constants'
 import { getFormattedDate } from '../modules/date'
 import useMediumZoom from '../composables/useMediumZoom'
 import useReadHistory from '../composables/useReadHistory'
+import Prose from '../components/Prose.vue'
 
 const { frontmatter } = useProjectFrontmatter()
 
@@ -86,48 +87,9 @@ if (frontmatter.value?.name) pushReadHistory(frontmatter.value?.name)
         </div>
       </div>
 
-      <article class="prose">
+      <Prose>
         <RouterView />
-      </article>
+      </Prose>
     </div>
   </div>
 </template>
-
-<style lang="sass" scoped>
-=media
-  max-width: min(30rem, 100%)
-
-:deep(.prose)
-  @apply mx-auto
-  h2
-    @apply mb-4 text-2xl font-medium md:text-3xl
-    &:not(:first-child)
-      @apply mt-12
-  h3
-    @apply mb-4 text-xl font-medium
-
-  p
-    @apply leading-7 mb-6
-
-  ul, ol
-    @apply list-outside pl-5 mb-6 space-y-1
-    li
-      @apply pl-2
-  ul
-    @apply list-disc
-  ol
-    @apply list-decimal
-
-  p > img
-    @apply mx-auto w-auto mb-4
-    +media
-  a
-    @apply underline
-  hr
-    @apply my-6 border-current opacity-30
-  .embed-responsive
-    @apply mb-12 mx-auto w-full relative overflow-hidden after:pt-[56.25%] after:block
-    +media
-    .embed-responsive-item
-      @apply absolute inset-0 h-full w-full
-</style>
