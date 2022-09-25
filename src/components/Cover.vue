8c0abb447c9a99e63af743035367491872be7dc4
ngseke <ngseke@gmail.com>
2022-09-25 11:47:27 +0800

    refactor: refactor by removing element id

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import CoverBody from './CoverBody.vue'
 import image from '../assets/img/cover-background.jpg'
+import useIsCoverVisible from '../composables/useIsCoverVisible'
 
 const style = {
   backgroundImage: `url(${image})`,
 }
+
+const { coverRef } = useIsCoverVisible()
 </script>
 
 <template>
   <header
-    id="cover"
+    ref="coverRef"
     class="
       flex min-h-screen flex-col justify-center
       bg-black bg-cover bg-center
--- src/composables/useIsCoverVisible.ts
+++ src/composables/useIsCoverVisible.ts
@@ -1,25 +1,28 @@
 import { useIntersectionObserver } from '@vueuse/core'
 
+const coverRef = ref<HTMLElement | null>(null)
+
 export default function useIsCoverVisible () {
   const route = useRoute()
   const isCoverVisible = ref<boolean | null>(null)
-  const $cover = ref<HTMLElement | null>(null)
 
   onMounted(() => {
     watch(route, async () => {
       if (!isClient) return
       await nextTick()
-      $cover.value = document.querySelector('#cover')
-      if (!$cover.value) isCoverVisible.value = null
+      if (!coverRef.value) isCoverVisible.value = null
     }, { immediate: true })
   })
 
   useIntersectionObserver(
-    $cover,
+    coverRef,
     ([{ isIntersecting }]) => {
       isCoverVisible.value = isIntersecting
     },
   )
 
-  return { isCoverVisible }
+  return {
+    coverRef,
+    isCoverVisible,
+  }
 }
