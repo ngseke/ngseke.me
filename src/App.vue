d859adc3a510852f9b30d8efd94390972c99f013
ngseke <ngseke@gmail.com>
2022-10-15 21:55:03 +0800

    feat: add nprogress

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import useMountedBodyClass from './composables/useMountedBodyClass'
+import useNProgress from './composables/useNProgress'
 useMountedBodyClass()
+useNProgress()
 </script>
 
 <template>
---
+++ src/composables/useNProgress.ts
@@ -0,0 +1,15 @@
+import NProgress from 'nprogress'
+
+export default function useNProgress () {
+  const { beforeEach, afterEach } = useRouter()
+
+  beforeEach((_to, _from, next) => {
+    NProgress.configure({
+      showSpinner: false,
+      trickleSpeed: 100,
+    }).start()
+    next()
+  })
+
+  afterEach(() => setTimeout(NProgress.done, 200))
+}
