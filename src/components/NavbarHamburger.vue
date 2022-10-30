f26133511d78f86e43ed82699147852c89f60909
ngseke <ngseke@gmail.com>
2022-10-30 15:30:39 +0800

    fix: fix list overflow style

--- src/components/NavbarHamburger.vue
+++ src/components/NavbarHamburger.vue
@@ -2,7 +2,7 @@
 import Link from '../types/Link'
 
 defineProps<{
-  links?: Link[],
+  links: Link[],
   show: boolean,
 }>()
 
@@ -18,7 +18,7 @@ defineEmits<{
         v-if="show"
         class="fixed inset-x-0 top-16 bottom-0 z-20 bg-black p-4 transition-all sm:hidden"
       >
-        <ul class="flex h-full flex-col items-center space-y-8 pt-12 text-2xl font-medium text-white/80">
+        <ul class="flex h-full flex-col items-center space-y-8 overflow-y-auto pt-12 text-2xl font-medium text-white/80">
           <li
             v-for="({ name, href, handler }, index) in links"
             :key="index"
