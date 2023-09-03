8cb4c82e0a0d666b67cc44eb40b94434405a5d13
ngseke <ngseke@gmail.com>
2023-09-03 08:20:07 +0800

    feat: add `startViewTransition when toggling dark mode

--- src/components/ButtonDarkMode.vue
+++ src/components/ButtonDarkMode.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import { useToggle } from '@vueuse/core'
 import { useDark } from '../composables/useDark'
+import { useRootViewTransition } from '../composables/useRootViewTransition'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
+
+const { startViewTransition } = useRootViewTransition()
+const handleClick = (event: MouseEvent) => {
+  startViewTransition(toggleDark, [event.clientX, event.clientY])
+}
 </script>
 
 <template>
   <ButtonRotateIcon
     :title="isDark ? '關閉深色模式' : '開啟深色模式'"
-    @click="toggleDark()"
+    @click="handleClick"
   >
     <Fa v-if="isDark" :icon="['fas', 'sun']" />
     <Fa v-else :icon="['fas', 'moon']" />
