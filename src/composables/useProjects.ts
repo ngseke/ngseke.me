2f7f7e0bda691c8d24fa693e2c558b9ddd31e2a8
ngseke <ngseke@gmail.com>
2023-10-16 12:38:38 +0800

    refactor: remove unused properties

--- src/composables/useProjects.ts
+++ src/composables/useProjects.ts
@@ -26,14 +26,12 @@ export function useProjects () {
     return [
       {
         title: 'Browser Extension',
-        cover: 'https://images.unsplash.com/photo-1541512416146-3cf58d6b27cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80',
         list: [
           map['leetcode-night'],
         ],
       },
       {
         title: 'Web',
-        cover: 'https://images.unsplash.com/photo-1505356822725-08ad25f3ffe4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
         list: [
           map['iphone-price'],
           map.koasu,
@@ -45,14 +43,12 @@ export function useProjects () {
       },
       {
         title: 'System Design',
-        cover: 'https://images.unsplash.com/photo-1505356822725-08ad25f3ffe4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
         list: [
           map.boss,
         ],
       },
       {
         title: 'Game',
-        cover: 'https://images.unsplash.com/photo-1528804431125-842f17de657b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1160&q=80',
         list: [
           {
             title: 'Flip Card',
@@ -69,7 +65,6 @@ export function useProjects () {
       },
       {
         title: 'Identity Design',
-        cover: 'https://images.unsplash.com/photo-1616941245386-fc2f3458836f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80',
         list: [
           map['camp-2017'],
           map.shanlinliang,
