954b2f75ed81e7a7320eb11cd775c16f9b7bda44
ngseke <ngseke@gmail.com>
2023-10-28 12:23:44 +0800

    feat: add `/fe` for downplay projects

---
+++ src/pages/fe.vue
@@ -0,0 +1,19 @@
+<route lang="yaml">
+name: "fe"
+</route>
+
+<script setup lang="ts">
+import { useDownplayProjects } from '../composables/useDownplayProjects'
+
+const { isDownplayed } = useDownplayProjects()
+isDownplayed.value = true
+const router = useRouter()
+
+onMounted(() => {
+  router.replace({ name: 'index' })
+})
+</script>
+
+<template>
+  <div />
+</template>
